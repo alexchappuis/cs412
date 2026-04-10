d8c6b3daf45f6b0f7b39973f56835e4620ddf9eb
Alex Chappuis <alexc26@bu.edu>
2026-04-10 14:48:28 -0400

    fix

--- cs412/settings.py
+++ cs412/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     "marathon_analytics",
     "voter_analytics",
     "rest_framework",
+    'rest_framework.authtoken',
     "dadjokes"
 ]
 
@@ -154,6 +155,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = "/media/"
 
 REST_FRAMEWORK = {
-  'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-  'PAGE_SIZE': 10
+    'DEFAULT_AUTHENTICATION_CLASSES': [ 
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+ #   'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+ #   'PAGE_SIZE': 10
 }
